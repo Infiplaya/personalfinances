899744ea7c5608889c9b08f6ce9aef6a60b6205c
Pawel Sobaniec <infipablosoban@gmail.com>
2023-08-18 14:48:10 +0200

    edit loading states

--- app/(dashboard)/loading.tsx
+++ app/(dashboard)/loading.tsx
@@ -1,27 +1,23 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
-  return (
-    <main className="container mx-auto w-full space-y-10 py-10">
-      <div className="lg:grid grid-cols-8 gap-x-10">
-        <div className="lg:col-span-2">
-          <Skeleton  className="w-[300px] h-[350px]"/>
-        </div>
-        <div className="lg:col-span-2">
-          <Skeleton  className="w-[300px] h-[350px]"/>
-        </div>
-        <div className="lg:col-span-4">
-          <Skeleton className="w-[600px] h-[350px]" />
-        </div>
-      </div>
-      <div className="grid lg:grid-cols-2 gap-x-10">
-        <div className="col-span-1">
-          <Skeleton className="w-[600px] h-[350px]" />
-        </div>
-        <div className="col-span-1">
-          <Skeleton className="w-[600px] h-[350px]" />
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main className="space-y-10 py-10">
+            <div className="grid-cols-8 gap-x-10 space-y-10 lg:space-x-10 lg:space-y-0 lg:grid">
+                <Skeleton className="h-64 w-full lg:col-span-2" />
+
+                <Skeleton className="h-64 w-full lg:col-span-2" />
+
+                <Skeleton className="h-64 w-full lg:col-span-4" />
+            </div>
+            <div className="grid gap-x-10 lg:grid-cols-2">
+                <div className="col-span-1">
+                    <Skeleton className="h-[350px] w-[600px]" />
+                </div>
+                <div className="col-span-1">
+                    <Skeleton className="h-[350px] w-[600px]" />
+                </div>
+            </div>
+        </main>
+    );
 }
