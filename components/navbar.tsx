a43bb7bd579391851b76d76dbd800dc55eb3bca3
Pawel Sobaniec <infipablosoban@gmail.com>
2023-08-19 16:42:53 +0200

    render navbar on server

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,18 @@
-'use client';
-
-import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 import { SignIn } from './auth/sign-in';
 import { SignOut } from './auth/sign-out';
 import { ThemeSwitcher } from './theme-switcher';
 import MobileNavbar from './mobile-navbar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth/auth';
 
-export default function Navbar() {
-  const { status } = useSession();
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
   return (
     <nav className="sticky top-0 inline-flex w-full items-center justify-end border-b border-gray-200 bg-white px-10 py-4 dark:border-gray-800 dark:bg-black">
       <div className="inline-flex space-x-8">
-        {status === 'authenticated' ? (
+        {session?.user ? (
           <SignOut />
         ) : (
           <div className="inline-flex items-center space-x-4">
