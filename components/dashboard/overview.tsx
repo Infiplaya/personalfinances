e0ea056cb97a297f2bfe9d10c1f1a7b608846252
Pawel Sobaniec <infipablosoban@gmail.com>
2023-08-19 17:01:53 +0200

    fix bug

--- components/dashboard/overview.tsx
+++ components/dashboard/overview.tsx
@@ -1,81 +1,81 @@
 'use client';
 
-import { OverviewData } from '@/app/(dashboard)/page';
+import { OverviewData } from '@/db/queries/transactions';
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 import { CardTitleWithTooltip } from './card-title-with-tooltip';
 
 export function Overview({ data }: { data: OverviewData }) {
-    return (
-        <div className="lg:flex lg:gap-10 space-y-10 lg:space-y-0">
-            <div className="flex-1">
-                <Card>
-                    <CardHeader>
-                        <CardTitleWithTooltip message="Your incomes in last 7 days.">
-                            <CardTitle>Incomes</CardTitle>
-                        </CardTitleWithTooltip>
-                    </CardHeader>
-                    <CardContent>
-                        <ResponsiveContainer width="100%" height={350}>
-                            <BarChart data={data}>
-                                <XAxis
-                                    dataKey="day"
-                                    stroke="#888888"
-                                    fontSize={12}
-                                    tickLine={false}
-                                    axisLine={false}
-                                />
-                                <YAxis
-                                    stroke="#888888"
-                                    fontSize={12}
-                                    tickLine={false}
-                                    axisLine={false}
-                                    tickFormatter={(value) => `$${value}`}
-                                />
-                                <Bar
-                                    dataKey="totalIncome"
-                                    fill="#22c55e"
-                                    radius={[4, 4, 0, 0]}
-                                />
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </CardContent>
-                </Card>
-            </div>
-            <div className="flex-1">
-                <Card>
-                    <CardHeader>
-                        <CardTitleWithTooltip message="Your spendings in last 7 days.">
-                            <CardTitle>Spendings</CardTitle>
-                        </CardTitleWithTooltip>
-                    </CardHeader>
-                    <CardContent>
-                        <ResponsiveContainer width="100%" height={350}>
-                            <BarChart data={data}>
-                                <XAxis
-                                    dataKey="day"
-                                    stroke="#888888"
-                                    fontSize={12}
-                                    tickLine={false}
-                                    axisLine={false}
-                                />
-                                <YAxis
-                                    stroke="#888888"
-                                    fontSize={12}
-                                    tickLine={false}
-                                    axisLine={false}
-                                    tickFormatter={(value) => `$${value}`}
-                                />
-                                <Bar
-                                    dataKey="totalExpenses"
-                                    fill="#ef4444"
-                                    radius={[4, 4, 0, 0]}
-                                />
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </CardContent>
-                </Card>
-            </div>
-        </div>
-    );
+  return (
+    <div className="space-y-10 lg:flex lg:gap-10 lg:space-y-0">
+      <div className="flex-1">
+        <Card>
+          <CardHeader>
+            <CardTitleWithTooltip message="Your incomes in last 7 days.">
+              <CardTitle>Incomes</CardTitle>
+            </CardTitleWithTooltip>
+          </CardHeader>
+          <CardContent>
+            <ResponsiveContainer width="100%" height={350}>
+              <BarChart data={data}>
+                <XAxis
+                  dataKey="day"
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <YAxis
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                  tickFormatter={(value) => `$${value}`}
+                />
+                <Bar
+                  dataKey="totalIncome"
+                  fill="#22c55e"
+                  radius={[4, 4, 0, 0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex-1">
+        <Card>
+          <CardHeader>
+            <CardTitleWithTooltip message="Your spendings in last 7 days.">
+              <CardTitle>Spendings</CardTitle>
+            </CardTitleWithTooltip>
+          </CardHeader>
+          <CardContent>
+            <ResponsiveContainer width="100%" height={350}>
+              <BarChart data={data}>
+                <XAxis
+                  dataKey="day"
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <YAxis
+                  stroke="#888888"
+                  fontSize={12}
+                  tickLine={false}
+                  axisLine={false}
+                  tickFormatter={(value) => `$${value}`}
+                />
+                <Bar
+                  dataKey="totalExpenses"
+                  fill="#ef4444"
+                  radius={[4, 4, 0, 0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
