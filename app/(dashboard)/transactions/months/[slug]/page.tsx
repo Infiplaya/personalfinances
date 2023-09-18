9205014ab01604b7362480947b35ecac532e223a
Pawel Sobaniec <infipablosoban@gmail.com>
2023-09-18 21:40:23 +0200

    add prop to slug page

--- app/(dashboard)/transactions/months/[slug]/page.tsx
+++ app/(dashboard)/transactions/months/[slug]/page.tsx
@@ -3,18 +3,36 @@ import { TransactionDialog } from '@/components/transactions/transaction-dialog'
 import { getCategories } from '@/db/queries/categories';
 import { getMonthIndex } from '@/lib/utils';
 import { TransactionByMonth } from './transactions-by-month';
+import {
+  getCurrencies,
+  getUserPrefferedCurrency,
+} from '@/db/queries/currencies';
 
-export default async function MonthPage({params}: {params: {slug: string}}) {
+export default async function MonthPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const categoriesData = await getCategories();
+  const currenciesData = await getCurrencies();
+  const currentCurrency = await getUserPrefferedCurrency();
 
-  console.log(getMonthIndex(params.slug))
+  console.log(getMonthIndex(params.slug));
 
   return (
     <main className="mx-auto py-10">
       <div className="flex w-full justify-end px-3 lg:my-6">
-        <TransactionDialog categories={categoriesData} />
+        <TransactionDialog
+          categories={categoriesData}
+          currencies={currenciesData}
+          currentCurrency={currentCurrency}
+        />
         <div className="lg:hidden">
-          <TransactionDrawer categories={categoriesData} />
+          <TransactionDrawer
+            categories={categoriesData}
+            currencies={currenciesData}
+            currentCurrency={currentCurrency}
+          />
         </div>
       </div>
       <TransactionByMonth month={getMonthIndex(params.slug)} />
