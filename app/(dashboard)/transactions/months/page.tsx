f4d7ef5ea8a9afd45d643aa14545b96f9a968634
Pawel Sobaniec <infipablosoban@gmail.com>
2023-09-18 21:47:36 +0200

    add prop

--- app/(dashboard)/transactions/months/page.tsx
+++ app/(dashboard)/transactions/months/page.tsx
@@ -1,13 +1,15 @@
 import MonthlyBalanceCard from '@/components/dashboard/month-summary-card';
+import { getUserPrefferedCurrency } from '@/db/queries/currencies';
 import { getSummariesForMonths } from '@/db/queries/transactions';
 
 export default async function MonthPage() {
   const monthData = await getSummariesForMonths();
+  const currentCurrency = await getUserPrefferedCurrency();
 
   return (
     <main className="mx-auto py-10">
       {monthData.map((m) => (
-        <MonthlyBalanceCard month={m} key={m.month} />
+        <MonthlyBalanceCard month={m} key={m.month} currencyCode={currentCurrency} />
       ))}
     </main>
   );
