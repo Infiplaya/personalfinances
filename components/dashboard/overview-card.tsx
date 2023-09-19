ce51d29e9c6a0ffcdf957fe11d4dee9694d6a229
Pawel Sobaniec <infipablosoban@gmail.com>
2023-09-19 15:59:13 +0200

    add overview card

---
+++ components/dashboard/overview-card.tsx
@@ -0,0 +1,54 @@
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { CardTitleWithTooltip } from './card-title-with-tooltip';
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { moneyFormat } from '@/lib/utils';
+import { OverviewData } from '@/db/queries/transactions';
+
+export function OverviewCard({
+  data,
+  dataKey,
+  currencyCode,
+  title,
+  message,
+}: {
+  data: OverviewData;
+  currencyCode: string;
+  dataKey: string;
+  title: string;
+  message: string;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitleWithTooltip message={message}>
+          <CardTitle>{title}</CardTitle>
+        </CardTitleWithTooltip>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={350}>
+          <BarChart data={data}>
+            <XAxis
+              dataKey="day"
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+            <YAxis
+              stroke="#888888"
+              fontSize={10}
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={(value) => `${moneyFormat(value, currencyCode)}`}
+            />
+            <Bar
+              dataKey={dataKey}
+              fill={title == 'Expenses' ? '#F4364C' : '#39E75F'}
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
+}
--- components/dashboard/overview.tsx
+++ components/dashboard/overview.tsx
@@ -2,9 +2,9 @@
 
 import { OverviewData } from '@/db/queries/transactions';
 import { moneyFormat } from '@/lib/utils';
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+
 import { CardTitleWithTooltip } from './card-title-with-tooltip';
+import { OverviewCard } from './overview-card';
 
 export function Overview({
   data,
@@ -13,78 +13,25 @@ export function Overview({
   data: OverviewData;
   currencyCode: string;
 }) {
-  console.log(data);
   return (
     <div className="space-y-10 lg:flex lg:gap-10 lg:space-y-0">
       <div className="flex-1">
-        <Card>
-          <CardHeader>
-            <CardTitleWithTooltip message="Your incomes in last 7 days.">
-              <CardTitle>Incomes</CardTitle>
-            </CardTitleWithTooltip>
-          </CardHeader>
-          <CardContent>
-            <ResponsiveContainer width="100%" height={350}>
-              <BarChart data={data}>
-                <XAxis
-                  dataKey="day"
-                  stroke="#888888"
-                  fontSize={12}
-                  tickLine={false}
-                  axisLine={false}
-                />
-                <YAxis
-                  stroke="#888888"
-                  fontSize={12}
-                  tickLine={false}
-                  axisLine={false}
-                  tickFormatter={(value) =>
-                    `${moneyFormat(value, currencyCode)}`
-                  }
-                />
-                <Bar
-                  dataKey="totalIncome"
-                  fill="#22c55e"
-                  radius={[4, 4, 0, 0]}
-                />
-              </BarChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
+        <OverviewCard
+          data={data}
+          currencyCode={currencyCode}
+          dataKey="totalExpenses"
+          title='Expenses'
+          message='Your expenses in last 7 days'
+        />
       </div>
       <div className="flex-1">
-        <Card>
-          <CardHeader>
-            <CardTitleWithTooltip message="Your spendings in last 7 days.">
-              <CardTitle>Spendings</CardTitle>
-            </CardTitleWithTooltip>
-          </CardHeader>
-          <CardContent>
-            <ResponsiveContainer width="100%" height={350}>
-              <BarChart data={data}>
-                <XAxis
-                  dataKey="day"
-                  stroke="#888888"
-                  fontSize={12}
-                  tickLine={false}
-                  axisLine={false}
-                />
-                <YAxis
-                  stroke="#888888"
-                  fontSize={12}
-                  tickLine={false}
-                  axisLine={false}
-                  tickFormatter={(value) => `$${value}`}
-                />
-                <Bar
-                  dataKey="totalExpenses"
-                  fill="#ef4444"
-                  radius={[4, 4, 0, 0]}
-                />
-              </BarChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
+        <OverviewCard
+          data={data}
+          currencyCode={currencyCode}
+          dataKey="totalIncome"
+          title='Incomes'
+          message='Your incomes in last 7 days'
+        />
       </div>
     </div>
   );
