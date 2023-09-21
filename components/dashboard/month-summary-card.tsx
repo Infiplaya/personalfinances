46f13a5d0011b8508923c5bb9c4ccf97b66f9a02
Pawel Sobaniec <infipablosoban@gmail.com>
2023-09-21 15:35:28 +0200

    fix types

--- components/dashboard/month-summary-card.tsx
+++ components/dashboard/month-summary-card.tsx
@@ -4,10 +4,10 @@ import { Label } from '../ui/label';
 import { CardTitleWithTooltip } from './card-title-with-tooltip';
 
 interface Month {
-  month: number;
-  totalIncomes: string;
-  totalExpenses: string;
-  totalBalance: string;
+  month: number | undefined;
+  totalIncomes: number;
+  totalExpenses: number;
+  totalBalance: number;
 }
 
 export default function MonthlyBalanceCard({
@@ -17,46 +17,50 @@ export default function MonthlyBalanceCard({
   month: Month;
   currencyCode: string;
 }) {
-  return (
-    <Card key={month.month}>
-      <CardHeader>
-        <CardTitleWithTooltip
-          link={`/transactions/months/${getMonth(month.month)}`}
-          message={`Summary of incomes and spendings in ${getMonth(
-            month.month,
-            true
-          )}`}
-        >
-          <CardTitle>{getMonth(month.month, true)}</CardTitle>
-        </CardTitleWithTooltip>
-      </CardHeader>
-      <CardContent className="space-y-3">
-        <div>
-          <Label>Income</Label>
-          <p className="text-lg font-semibold">
-            {moneyFormat(Number(month.totalIncomes), currencyCode)}
-          </p>
-        </div>
-        <div>
-          <Label>Expenses</Label>
-          <p className="text-lg font-semibold">
-            {moneyFormat(Number(month.totalExpenses), currencyCode)}
-          </p>
-        </div>
-        <div>
-          <Label>Balance</Label>
-          <p
-            className={cn(
-              'text-lg font-semibold',
-              Number(month.totalBalance) > 0
-                ? 'text-green-500 dark:text-green-400'
-                : 'text-red-500 dark:text-red-400'
-            )}
+  if (month.month) {
+    return (
+      <Card key={month.month}>
+        <CardHeader>
+          <CardTitleWithTooltip
+            link={`/transactions/months/${getMonth(month.month)}`}
+            message={`Summary of incomes and spendings in ${getMonth(
+              month.month,
+              true
+            )}`}
           >
-            {moneyFormat(Number(month.totalBalance), currencyCode)}
-          </p>
-        </div>
-      </CardContent>
-    </Card>
-  );
+            <CardTitle>{getMonth(month.month, true)}</CardTitle>
+          </CardTitleWithTooltip>
+        </CardHeader>
+        <CardContent className="space-y-3">
+          <div>
+            <Label>Income</Label>
+            <p className="text-lg font-semibold">
+              {moneyFormat(Number(month.totalIncomes), currencyCode)}
+            </p>
+          </div>
+          <div>
+            <Label>Expenses</Label>
+            <p className="text-lg font-semibold">
+              {moneyFormat(Number(month.totalExpenses), currencyCode)}
+            </p>
+          </div>
+          <div>
+            <Label>Balance</Label>
+            <p
+              className={cn(
+                'text-lg font-semibold',
+                Number(month.totalBalance) > 0
+                  ? 'text-green-500 dark:text-green-400'
+                  : 'text-red-500 dark:text-red-400'
+              )}
+            >
+              {moneyFormat(Number(month.totalBalance), currencyCode)}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return null;
 }
