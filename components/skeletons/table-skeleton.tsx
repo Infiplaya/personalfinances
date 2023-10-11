f0edbcd14019735108d51f2bbe86a43f88f6ad96
Pawel Sobaniec <infipablosoban@gmail.com>
2023-10-11 17:56:29 +0200

    fix loading

--- components/skeletons/table-skeleton.tsx
+++ components/skeletons/table-skeleton.tsx
@@ -1,16 +1,12 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function TableSkeleton() {
+  const skeletonElements = Array.from({ length: 10 }, (_, index) => (
+    <Skeleton key={index} className="h-8 lg:w-[1250px]" />
+  ));
   return (
     <div>
-      <div className="mt-12 px-3 space-y-6">
-        <Skeleton className="h-8 lg:w-[1250px]" />
-        <Skeleton className="h-8 lg:w-[1250px]" />
-        <Skeleton className="h-8 lg:w-[1250px]" />
-        <Skeleton className="h-8 lg:w-[1250px]" />
-        <Skeleton className="h-8 lg:w-[1250px]" />
-        <Skeleton className="h-8 lg:w-[1250px]" />
-      </div>
+      <div className="mt-12 space-y-6 px-3">{skeletonElements}</div>
     </div>
   );
 }
