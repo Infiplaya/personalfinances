51d7b37e125d51b6f1a9b18314eda522143c1ef0
Pawel Sobaniec <infipablosoban@gmail.com>
2023-10-25 22:59:24 +0200

    add optimistic updates to columns

--- app/(dashboard)/budget/board.tsx
+++ app/(dashboard)/budget/board.tsx
@@ -12,8 +12,6 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-
-import { Skeleton } from '@/components/ui/skeleton';
 import { Columns } from '@/db/queries/budgets';
 import { cn } from '@/lib/utils';
 import { useEffect, useRef, useState, useTransition } from 'react';
@@ -21,13 +19,15 @@ import { DragDropContext, Draggable, DropResult } from 'react-beautiful-dnd';
 // @ts-expect-error
 import { experimental_useFormState as useFormState } from 'react-dom';
 import { StrictModeDroppable } from './strict-droppable';
-import { Plus, PlusCircle } from 'lucide-react';
+import { PlusCircle } from 'lucide-react';
 
 import { Separator } from '@/components/ui/separator';
 import { ChangeColumnName } from './change-column-name';
 import ColumnOptions from './column-options';
 import { toast } from 'sonner';
 import { SubmitButton } from '../settings/submit-button';
+import { experimental_useOptimistic as useOptimistic } from 'react';
+import { BudgetPlan, BudgetStatus } from '@/db/schema/finances';
 
 const initialState = {
   success: null,
@@ -35,11 +35,10 @@ const initialState = {
 };
 
 export function Board({ data }: { data: Columns }) {
-  const [isPending, startTransition] = useTransition();
   const [editedColumn, setEditedColumn] = useState<null | string>(null);
   const [displayForm, setDisplayForm] = useState(false);
   const nameInputRef = useRef<null | HTMLInputElement>(null);
-
+  const [optimisticColumns, changeColumnItems] = useOptimistic<Columns>(data);
   const [state, newPlanAction] = useFormState(createBudgetPlan, initialState);
 
   function resetForm() {
@@ -71,18 +70,36 @@ export function Board({ data }: { data: Columns }) {
       const [removed] = sourceItems.splice(source.index, 1);
       destItems.splice(destination.index, 0, removed);
 
-      startTransition(async () => {
-        await updateBudgetPlanStatus(removed.id, destColumn.id);
+      changeColumnItems({
+        ...optimisticColumns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          budgetPlans: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          budgetPlans: destItems,
+        },
       });
+
+      await updateBudgetOrder(destItems);
+
+      await updateBudgetPlanStatus(removed.id, destColumn.id);
     } else {
       const column = data[source.droppableId];
       const copiedItems = [...column.budgetPlans];
       const [removed] = copiedItems.splice(source.index, 1);
       copiedItems.splice(destination.index, 0, removed);
 
-      startTransition(async () => {
-        await updateBudgetOrder(copiedItems);
+      changeColumnItems({
+        ...optimisticColumns,
+        [source.droppableId]: {
+          ...column,
+          budgetPlans: copiedItems,
+        },
       });
+
+      await updateBudgetOrder(copiedItems);
     }
   };
 
@@ -91,10 +108,11 @@ export function Board({ data }: { data: Columns }) {
     setDisplayForm(true);
   }
 
+  console.log(optimisticColumns);
   return (
     <div className="mt-10 flex gap-10 overflow-auto">
       <DragDropContext onDragEnd={async (result) => onDragEnd(result)}>
-        {Object.entries(data).map(([columnId, column]) => {
+        {Object.entries(optimisticColumns).map(([columnId, column]) => {
           return (
             <div className="w-full rounded-md bg-white/5 py-4" key={columnId}>
               <div className="mb-4 flex items-center justify-between px-6">
@@ -112,89 +130,85 @@ export function Board({ data }: { data: Columns }) {
               </div>
               <Separator />
 
-              {isPending ? (
-                <Skeleton className="mt-4 h-[500px] w-full rounded-md opacity-50" />
-              ) : (
-                <StrictModeDroppable droppableId={columnId} key={columnId}>
-                  {(provided, snapshot) => {
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        className={cn(
-                          'mt-4 min-h-[500px] w-full rounded-md lg:p-4',
-                          snapshot.isDraggingOver &&
-                            'bg-gray-100 dark:bg-gray-900'
-                        )}
-                      >
-                        {displayForm && editedColumn === columnId ? (
-                          <Card
-                            className={cn(
-                              'mb-5 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800'
-                            )}
-                          >
-                            <CardHeader>
-                              <form
-                                action={newPlanAction}
-                                className="flex items-center space-x-2"
-                              >
-                                <Input
-                                  type="text"
-                                  placeholder="Type name of plan..."
-                                  id="name"
-                                  name="name"
-                                  ref={nameInputRef}
-                                  autoFocus
-                                />
-                                <Input
-                                  type="hidden"
-                                  name="columnId"
-                                  id="columnId"
-                                  value={column.id}
-                                />
-                                <SubmitButton>Add</SubmitButton>
-                              </form>
-                            </CardHeader>
-                          </Card>
-                        ) : null}
-
-                        {column.budgetPlans.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={String(item.id)}
-                              index={index}
+              <StrictModeDroppable droppableId={columnId} key={columnId}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      className={cn(
+                        'mt-4 min-h-[500px] w-full rounded-md lg:p-4',
+                        snapshot.isDraggingOver &&
+                          'bg-gray-100 dark:bg-gray-900'
+                      )}
+                    >
+                      {displayForm && editedColumn === columnId ? (
+                        <Card
+                          className={cn(
+                            'mb-5 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800'
+                          )}
+                        >
+                          <CardHeader>
+                            <form
+                              action={newPlanAction}
+                              className="flex items-center space-x-2"
                             >
-                              {(provided, snapshot) => {
-                                return (
-                                  <Card
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    className={cn(
-                                      'mb-5 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800',
-                                      snapshot.isDragging &&
-                                        'bg-gray-100 dark:bg-gray-800'
-                                    )}
-                                  >
-                                    <CardHeader>
-                                      <CardTitle>{item.name}</CardTitle>
-                                      <CardDescription>
-                                        {item.description}
-                                      </CardDescription>
-                                    </CardHeader>
-                                  </Card>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </StrictModeDroppable>
-              )}
+                              <Input
+                                type="text"
+                                placeholder="Type name of plan..."
+                                id="name"
+                                name="name"
+                                ref={nameInputRef}
+                                autoFocus
+                              />
+                              <Input
+                                type="hidden"
+                                name="columnId"
+                                id="columnId"
+                                value={column.id}
+                              />
+                              <SubmitButton>Add</SubmitButton>
+                            </form>
+                          </CardHeader>
+                        </Card>
+                      ) : null}
+
+                      {column.budgetPlans.map((item, index) => {
+                        return (
+                          <Draggable
+                            key={item.id}
+                            draggableId={String(item.id)}
+                            index={index}
+                          >
+                            {(provided, snapshot) => {
+                              return (
+                                <Card
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  className={cn(
+                                    'mb-5 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800',
+                                    snapshot.isDragging &&
+                                      'bg-gray-100 dark:bg-gray-800'
+                                  )}
+                                >
+                                  <CardHeader>
+                                    <CardTitle>{item.name}</CardTitle>
+                                    <CardDescription>
+                                      {item.description}
+                                    </CardDescription>
+                                  </CardHeader>
+                                </Card>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </StrictModeDroppable>
             </div>
           );
         })}
